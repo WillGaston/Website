d273d6b6cbbb90747dcda78389c0f564f34a5490
William Gaston <z5591798@ad.unsw.edu.au>
2025-08-03 20:20:40 +1000

    added semicolon

--- components/Reviews.tsx
+++ components/Reviews.tsx
@@ -1,10 +1,18 @@
+import ReviewContent from "./ReviewContent";
+import ReviewDetails from "./ReviewDetails";
+//import ReviewHero from "./ReviewHero";
+// import ReviewTLDR from "./ReviewTLDR";
 
 export default function ReviewsPage() {
     
     return (
         <div className="flex flex-col items-center w-screen h-screen">
-            <div className='py-16 mx-auto text-center flex flex-col items-center'>
-                <h2>This page is a work in progess...</h2>
+            <div className=" flex flex-col items-center w-screen">
             </div>
-        </div>)
+            <div className="flex flex-row w-screen">
+                <div className="flex w-1/5 px-4 py-8 items-center justify-center"><ReviewDetails /></div>
+                <div className="flex w-4/5 px-4 py-8"><ReviewContent /></div>
+            </div>
+        </div>
+    );
 }
