86c5c2ad4c1ebb1e8808c33b595621fa1b20c50d
William Gaston <z5591798@ad.unsw.edu.au>
2025-11-25 17:33:27 +1100

    continued refactor

---
+++ components/PrimaryPages/GalleryPage.tsx
@@ -0,0 +1,25 @@
+import Photo from '@/components/Photos';
+
+
+export default function GalleryPage() {
+  return (
+    <div className="flex flex-col items-center w-screen h-screen">
+      <div className = 'py-16 mx-auto text-center flex flex-col items-center'>
+        <h1 className = 'text-5xl font-bold font-mono tracking-tighter text-gray-800'>
+          My {' '}
+          <span className='text-orange-500'>
+            Photos
+          </span>
+          .
+        </h1>
+      </div>
+      <div className = 'py-4 mx-auto text-center font-mono tracking-tighter flex flex-col items-center text-xl'>
+        Below are various photos I have taken recently.
+        <Photo />
+      </div>
+      
+      
+    
+
+    </div>)
+}
